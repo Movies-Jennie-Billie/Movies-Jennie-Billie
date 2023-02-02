727b25b6f4bdcc28207b4f1decb5a52830bf281d
Jennie Smith <Jsmith9spiral@gmail.com>
2023-02-02 13:33:20 -0600

    update

--- movies.js
+++ movies.js
@@ -6,20 +6,24 @@
             id: 7,
             title: 'yay',
             rating: 4,
+            director: "Jimbobyo",
             genre: "sci-fi"
         };
         return movieObj
     }
 
     const url = 'https://brook-pale-army.glitch.me/movies';
-    // const options = {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify(createMovie()),
-    // };
-    // fetch(url, options)
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(createMovie()),
+    };
+    fetch(url, options)
+        .then(response => response.json()) /* review was created successfully */
+        .then((json) => json)
+        .catch( error => console.error(error) );
 
     fetch(url, {method: "GET"})
         .then(response => response.json()) /* review was created successfully */
@@ -53,7 +57,7 @@
         html += `<li>Genre: ${data.genre}</li>`
         html += `</ul>`
         html += `</div>`
-        html += `<a href="#" class="btn btn-primary">Go somewhere</a>`
+        html += `<a href="#" data-id="${data.id}" class="btn btn-delete btn-primary">Go somewhere</a>`
         html+= `</div> </div>`
         return html
     }
@@ -69,4 +73,11 @@
         fetch(`${url}/${targetID}`, {method: "DELETE"})
         .then(response => response.json())
     }
+    $(".btn-delete").click( function (){
+        console.log($(this).attr("data-id"))
+    //    get button id
+    //    get matching card id
+    //    get whole card
+    //    then send card to deleteCard
+    })
 })();
